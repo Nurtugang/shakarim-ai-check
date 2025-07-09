72daa3fbd79c3db70e8ca2d7fb08362d9b6fe7ff
Nurtugang <nurtugang17@gmail.com>
2025-07-09 11:22:05 +0500

    Remove gemini_config.py from git tracking

--- shakarim_ai_check/gemini_config.py
+++
@@ -1,9 +0,0 @@
-import os
-from google import genai
-from dotenv import load_dotenv
-from google.genai import types
-
-load_dotenv()
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-client = genai.Client(api_key=GEMINI_API_KEY)
